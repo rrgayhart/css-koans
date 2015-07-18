c9fba77167e5c7b6718655baf1a6730b1d212648
Meeka <rrgayhart@gmail.com>
2015-07-18 14:35:05 -0600

    Adds codepen fork button

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -3,4 +3,8 @@ class Problem < ActiveRecord::Base
   validates_presence_of :codepen_username
 
   belongs_to :koan
+
+  def codepen_permalink
+    "http://codepen.io/#{codepen_username}/pen/#{codepen_slug}/"
+  end
 end
